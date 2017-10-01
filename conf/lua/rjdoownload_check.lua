43279dd4bb74a76a6d56989e4a1c3fa4cb5d4b0e
Administrator <zaq19999@163.com>
2017-10-01 18:21:37 +0800

    add

---
+++ conf/lua/rjdoownload_check.lua
@@ -0,0 +1,13 @@
+local path = '127.0.0.1:8621'
+
+local args = ngx.req.get_uri_args()
+
+local target_uri  = ngx.var.uri
+local target_args = ngx.var.args
+
+ngx.var.backend_host = path
+
+ngx.req.set_uri(target_uri)
+if target_args ~= nil then
+    ngx.req.set_uri_args(target_args)
+end
